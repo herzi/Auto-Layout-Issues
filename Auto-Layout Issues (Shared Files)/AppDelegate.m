9ea43bd557db614e066c6479f0344d563234fdc8
Sven Herzberg <herzberg.sven@googlemail.com>
2014-03-15 13:12:04 +0100

    Add the “root view only” application to the project

--- Auto-Layout Issues (Shared Files)/AppDelegate.m
+++ Auto-Layout Issues (Shared Files)/AppDelegate.m
@@ -8,14 +8,15 @@
 
 #import "AppDelegate.h"
 
+@interface AppDelegate ()
+
+@end
+
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
     return YES;
 }
 
