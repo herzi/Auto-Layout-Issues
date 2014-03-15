77a1cda326c5044a9b10267ea7c92e4dfde71a44
Sven Herzberg <herzberg.sven@googlemail.com>
2014-03-15 13:36:13 +0100

    Add a custom view controller to each application

---
+++ Auto-Layout Issues (Shared Files)/RootViewController.h
@@ -0,0 +1,13 @@
+//
+//  RootViewController.h
+//  Auto-Layout Issues
+//
+//  Created by Sven Herzberg on 15.03.14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RootViewController : UIViewController
+
+@end
